8047aa7746f7e4ef00122a088df0eaab0ef65e9f
PollemAnt <andrzejsobierajskiant@gmail.com>
2022-08-24 16:43:56 +0200

    Make needed variables

--- app/src/main/java/com/example/blescanner/Constants.kt
+++ app/src/main/java/com/example/blescanner/Constants.kt
@@ -1,5 +1,9 @@
 package com.example.blescanner
 
+import java.util.*
+
 object Constants {
-    const val
+    val BLINKY_SERVICE_UUID: UUID = UUID.fromString("de8a5aac-a99b-c315-0c80-60d4cbb51224")
+    val DIODE_UUID: UUID = UUID.fromString("5b026510-4088-c297-46d8-be6c736a087a")
+    val BUTTON_STATE_UUID: UUID = UUID.fromString("61a885a4-41c3-60d0-9a53-6d652a70d29c")
 }
