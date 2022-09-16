b3b443f583c03777a2f778336e332928f3fd1510
PollemAnt <andrzejsobierajskiant@gmail.com>
2022-09-16 16:24:21 +0200

    Add needed variables

--- app/src/main/java/com/example/blescanner/Constants.kt
+++ app/src/main/java/com/example/blescanner/Constants.kt
@@ -9,6 +9,11 @@ object Constants {
     val BUTTON_STATE_UUID: UUID = UUID.fromString("61a885a4-41c3-60d0-9a53-6d652a70d29c")
     val MESH_SERVICE_PARCEL_UUID: ParcelUuid =
         ParcelUuid.fromString("00001827-0000-1000-8000-00805f9b34fb")
+    val MESH_SILICON_LABS_PARCEL_UUID: ParcelUuid =
+        ParcelUuid.fromString("00001828-0000-1000-8000-00805f9b34fb")
     val MESH_SERVICE_UUID: UUID = UUID.fromString("00001827-0000-1000-8000-00805f9b34fb")
-
+    val MESH_PROVISION_CHARACTERISTIC_UUID: UUID =
+        UUID.fromString("00002adc-0000-1000-8000-00805f9b34fb")
+    val MESH_SILICON_LABS_CHARACTERISTIC_UUID: UUID =
+        UUID.fromString("00002ade-0000-1000-8000-00805f9b34fb")
 }
